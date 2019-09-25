74d81a3a9baf7d17093884f400923a748869820e
josh <48034855+JDJoffe@users.noreply.github.com>
2019-09-25 13:29:48 +1000

    initial

---
+++ Assets/Scripts/GameDataWriter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class GameDataWriter
+{
+    #region writing
+    private BinaryWriter writer;
+    public GameDataWriter(BinaryWriter writer)
+    {
+        this.writer = writer;
+    }
+    // you can have two functions with the same name as long as the parameters are a different type
+    public void Write(float value)
+    {
+        writer.Write(value);
+    }
+    public void Write(int value)
+    {
+        writer.Write(value);
+    }
+    public void Write(Quaternion value)
+    {
+        writer.Write(value.x);
+        writer.Write(value.y);
+        writer.Write(value.z);
+        writer.Write(value.w);
+    }
+    public void Write(Vector3 value)
+    {
+        writer.Write(value.x);
+        writer.Write(value.y);
+        writer.Write(value.z);
+    }
+    #endregion  
+}
+
+public class GameDataReader
+{
+    #region reading
+    private BinaryReader reader;
+    public GameDataReader(BinaryReader reader)
+    {
+        this.reader = reader;
+    }
+    // cannot have the same name as there are no parameters
+    public float ReadFloat()
+    {
+        return reader.ReadSingle();
+    }
+    public int ReadInt()
+    {
+        return reader.ReadInt32();
+    }
+    public Quaternion ReadQuarternion()
+    {
+        Quaternion value;
+        value.x = reader.ReadSingle();
+        value.y = reader.ReadSingle();
+        value.z = reader.ReadSingle();
+        value.w = reader.ReadSingle();
+
+        return value;
+    }
+    public Vector3 ReadVector3()
+    {
+        Vector3 value;
+        value.x = reader.ReadSingle();
+        value.y = reader.ReadSingle();
+        value.z = reader.ReadSingle();
+        return value;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : PersistableObject
+{
+    // just the position 
+    public PersistableObject prefab;
+    public Transform prefabParent;
+    public List<PersistableObject> cubes;
+    public PersistentStorage storage;
+    private string savePath;
+    // keycode variable, could maybe use this for changeable controls
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+    private void Awake()
+    {
+        cubes = new List<PersistableObject>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            // call function
+            CreateObject();
+        }
+        else if (Input.GetKeyDown(newGameKey))
+        {
+            // call function
+            NewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            // call function from storage
+            storage.Save(this);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            // call function from this class then from storage
+            NewGame();
+            storage.Load(this);
+        }
+    }
+    #region creating and clearing objects in list
+    private void CreateObject()
+    {
+       // Vector3 prevpos;
+        PersistableObject o = Instantiate(prefab, prefabParent);
+        // random.insideunitsphere makes the cubes spawn randomly within the sphere, 
+        // they may jut out but their centrepoint is never outside the sphere
+        #region diagram
+        //              +5
+        //              |
+        //              |
+        //       -5-----+-----+5
+        //              |
+        //              |
+        //              -5
+        #endregion     
+       // foreach (var cube in cubes)
+      //  {
+         //   if (true)
+         //   {
+                o.transform.localPosition = Random.insideUnitSphere * 5f;
+                o.transform.localRotation = Random.rotation;
+                o.transform.localScale = Random.Range(01, 1) * Vector3.one;
+                cubes.Add(o);
+        //    }
+         //  prevpos = cube.transform.position;
+       // }
+       
+    }
+    private void NewGame()
+    {
+        // var is a transform
+        foreach (var cube in cubes)
+        {
+            // destroy selected gameobject cube transform is connected to
+            Destroy(cube.gameObject);
+        }
+        cubes.Clear();
+    }
+    #endregion
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(Vector3.zero, 5f);
+    }
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(cubes.Count);
+        foreach (var cube in cubes)
+        {
+            cube.Save(writer);
+        }
+    }
+    public override void Load(GameDataReader reader)
+    {
+        int count = reader.ReadInt();
+        for (int i = 0; i < count; i++)
+        {
+            PersistableObject o = Instantiate(prefab,prefabParent);
+            o.Load(reader);
+            cubes.Add(o);
+        }
+    }
+}
+
